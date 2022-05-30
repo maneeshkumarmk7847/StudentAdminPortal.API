c11c2baf35e5702e661d4b7339e4b1e83ba4e5de
maneeshkumarmk7847 <manishkumarmk7847@gmail.com>
2022-05-30 11:53:45 +0530

    Add project files.

---
+++ StudentAdminPortal.API/DataModels/StudentAdminControl.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentAdminPortal.API.DataModels
+{
+    public class StudentAdminContext: DbContext
+    {
+        public StudentAdminContext(DbContextOptions<StudentAdminContext> options) : base(options)
+        {
+        }
+        public DbSet<Student>Student { get; set; }
+        public DbSet<Student> Gender { get; set; }
+        public DbSet<Student> Address { get; set; }
+    }
+}
